a57798081d14250a277cb13176c49855caac965c
Aaron Newman <63217782+aaronjnewman@users.noreply.github.com>
2025-08-11 15:43:01 -0300

    minor edits

--- syllabus2pdf.sh
+++ syllabus2pdf.sh
@@ -1,9 +1,14 @@
-# pandoc syllabus.md -f markdown -t pdf -o syllabus.pdf -V colorlinks=true \
+#!/bin/bash
+
+pandoc syllabus.md -f markdown -t pdf -o syllabus.pdf -V colorlinks=true \
+    -V linkcolor=blue \
+    -V urlcolor=blue \
+    -V toccolor=gray
+
+# pandoc syllabus_5001.md -f markdown -t pdf -o syllabus_5001.pdf -V colorlinks=true \
 #     -V linkcolor=blue \
 #     -V urlcolor=blue \
 #     -V toccolor=gray
 
-pandoc syllabus_5001.md -f markdown -t pdf -o syllabus_5001.pdf -V colorlinks=true \
-    -V linkcolor=blue \
-    -V urlcolor=blue \
-    -V toccolor=gray
+exit 0
+#
